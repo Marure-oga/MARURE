2dbff90df6226f60d11579ee65f6504f7cfaba82
Marure-oga <ogakiyusuke@Mac-C07ML21MDWYL.local>
2014-08-04 23:38:40 +0900

    Marure-oga.ver2
    ネットワークに接続しているかを確認する機能をつけました

--- Ratings/ViewController.h
+++ Ratings/ViewController.h
@@ -1,6 +1,12 @@
 
 #import <UIKit/UIKit.h>
 
+#import "Reachability.h"
+
+@class Reachability;
+
+
 @interface ViewController : UIViewController
+
 
 @end
--- Ratings/ViewController.m
+++ Ratings/ViewController.m
@@ -7,6 +7,10 @@
 
 @implementation ViewController
 NSTimer *ter;
+NSInteger event=0;
+NSInteger atomosphere = 0;
+double progresslevel = 0;
+UIProgressView *progressView;
 - (void)viewDidLoad
 {
     [super viewDidLoad];
@@ -17,20 +21,27 @@ NSTimer *ter;
     //CGContextSetRGBFillColor(context,0.0,0.0,1.0,1.0);
     //CGContextFillRect(context,CGRectMake(50,50,100,100));
     
-    ter = [NSTimer scheduledTimerWithTimeInterval:1
-                                           target:self
-                                         selector:@selector(nextPage:)
-                                         userInfo:nil
-                                          repeats:NO];
     
-    UIProgressView *progressView = [[UIProgressView alloc] initWithProgressViewStyle:UIProgressViewStyleDefault ];
+    progressView = [[UIProgressView alloc] initWithProgressViewStyle:UIProgressViewStyleDefault ];
     CGSize pSize = progressView.frame.size;
     CGSize vSize = self.view.frame.size;
     progressView.frame = CGRectMake((vSize.width-pSize.width)/2,(vSize.height-pSize.height)/2+100,pSize.width,pSize.height);
     progressView.progress = 0.0;
-    [progressView setProgress:1.0 animated:YES];
     progressView.trackTintColor = [UIColor blackColor];
+    [progressView setProgress:progresslevel animated:YES];
     [self.view addSubview:progressView];
+    
+    
+    [self syokika];
+    
+    [self network];
+    
+    [progressView setProgress:progresslevel animated:YES];
+    [self.view addSubview:progressView];
+    
+    
+    
+    
 }
 
 - (void)didReceiveMemoryWarning
@@ -39,6 +50,89 @@ NSTimer *ter;
     // Dispose of any resources that can be recreated.
 }
 
+
+//初期化用
+-(void)syokika
+{
+    event = 0;
+    atomosphere = 0;
+    progresslevel = progresslevel + 0.5;
+}
+
+//ネットワーク接続判定の中枢
+-(void)network
+{
+    Boolean accessstate = false;
+    
+    accessstate = [self networksearch];
+    [self networkaccessHantei:accessstate];
+}
+
+
+//ネットワーク検索用
+-(Boolean)networksearch
+{
+    Boolean networkaccess = false;
+    
+    Reachability *curReach = [Reachability reachabilityForInternetConnection];
+    NetworkStatus netStatus =[curReach currentReachabilityStatus];
+    
+    if(netStatus == NotReachable){
+        networkaccess = false;
+    }else{
+        networkaccess = true;
+        progresslevel = progresslevel + 0.5;
+        [progressView setProgress:progresslevel animated:YES];
+        [self.view addSubview:progressView];
+    }
+    
+    return networkaccess;
+}
+
+//アラートの表示
+-(void)showAlert
+{
+    UIAlertView *alert =[[UIAlertView alloc]
+                         initWithTitle:@"エラー"
+                         message:@"ネットワークに接続していません\n再試行しますか？"
+                         delegate:self
+                         cancelButtonTitle:@"いいえ"
+                         otherButtonTitles:@"はい",
+                         nil];
+    alert.alertViewStyle = UIAlertViewStyleDefault;
+    [alert show];
+
+}
+
+//アラートのボタンが押されたときの処理
+-(void)alertView:(UIAlertView*)alertView
+clickedButtonAtIndex:(NSInteger)buttonIndex{
+    switch(buttonIndex){
+        case 0:
+            exit(1);
+            break;
+        case 1:
+            [self network];
+            break;
+    }
+}
+
+//ネットワーク接続ができているかどうか
+-(void)networkaccessHantei:(Boolean)accessstate
+{
+    if(accessstate){
+        ter = [NSTimer scheduledTimerWithTimeInterval:1.2
+                                               target:self
+                                             selector:@selector(nextPage:)
+                                             userInfo:nil
+                                              repeats:NO];
+    }else{
+        [self showAlert];
+    }
+
+}
+
+//次の画面へ遷移
 -(void)nextPage:(NSTimer*)timer{
     ViewController *viewCont =[self.storyboard instantiateViewControllerWithIdentifier:@"main"];
     //[self.navigationItem setHidesBackButton:YES animated:YES];
