cfb2f064dadfebdb14ae7dc57844a5743cd7aaf7
chaosyyf <yangyifan@youitsuho-no-MacBook-Air.local>
2014-09-04 23:38:47 +0900

    JSONファイル仕様変更、webapi結果格納仕様変更、コメント追加

--- Ratings/JsonSerchClass.h
+++ Ratings/JsonSerchClass.h
@@ -8,23 +8,38 @@
 #import <Foundation/Foundation.h>
 @interface MarureKeyS : NSObject
 {
+    //一時格納用変数
+    NSString *eventName;                        //イベント名
+    NSString *ekey1,*ekey2,*ekey3,*ekey4;       //イベントに紐付けキーワード
+    NSString *moodyName;                        //雰囲気名
+    NSString *mkey1,*mkey2,*mkey3,*mkey4;       //雰囲気名に紐付けキーワード
+    //単一配列　破棄
+    //NSMutableArray *recipeNameArr ,*recipeImgArr,*recipUrlArr;
     
-    NSString *eventName;
-    NSString *ekey1,*ekey2,*ekey3;
-    NSString *moodyName;
-    NSString *mkey1,*mkey2,*mkey3;
-    NSMutableArray *recipeNameArr ,*recipeImgArr,*recipUrlArr;
+    //結果格納する配列
+    NSMutableArray *key1NameArr,*key1ImgArr,*key1UrlArr;
+    NSMutableArray *key2NameArr,*key2ImgArr,*key2UrlArr;
+    NSMutableArray *key3NameArr,*key3ImgArr,*key3UrlArr;
+    NSMutableArray *key4NameArr,*key4ImgArr,*key4UrlArr;
 }
 
 
-
+//破棄
 //@property(assign) NSString *eventName;
 //@property(assign) NSString *ekey1,*ekey2,*ekey3;
 //@property(assign) NSString *moodyName;
 //@property(assign) NSString *mkey1,*mkey2,*mkey3;
-@property NSMutableArray *recipeNameArr ,*recipeImgArr,*recipUrlArr;
+//@property NSMutableArray *recipeNameArr ,*recipeImgArr,*recipUrlArr;
+
+//外で呼び出せるように宣言
+@property NSMutableArray *key1NameArr,*key1ImgArr,*key1UrlArr;//キーワード１に関するすべての情報
+@property NSMutableArray *key2NameArr,*key2ImgArr,*key2UrlArr;//キーワード２に関するすべての情報
+@property NSMutableArray *key3NameArr,*key3ImgArr,*key3UrlArr;//キーワード３に関するすべての情報
+@property NSMutableArray *key4NameArr,*key4ImgArr,*key4UrlArr;//キーワード４に関するすべての情報
 
 
+//イベントと雰囲気をセットする
 -(void)SetEventAndMoody:(NSInteger)eventindex moody:(NSInteger)moodyindex;
--(void)WebSerchApi:(NSString *)keyword;
+//WEBAPI検索
+-(void)WebSerchApi:(NSString *)keyword nameArr:(NSMutableArray *)nameArr imgArr:(NSMutableArray *)imgArr urlArr:(NSMutableArray *)urlArr;
 @end
--- Ratings/JsonSerchClass.m
+++ Ratings/JsonSerchClass.m
@@ -8,52 +8,83 @@
 
 
 #import "JsonSerchClass.h"
-//url https://app.rakuten.co.jp/services/api/Recipe/CategoryRanking/20121121?format=json&categoryId=10&applicationId=1055797974705259361
 @implementation MarureKeyS
 
+//破棄
 //@synthesize eventName,ekey1,ekey2,ekey3,moodyName,mkey1,mkey2,mkey3,recipeArr;
-@synthesize recipeNameArr ,recipeImgArr,recipUrlArr;
+//@synthesize recipeNameArr ,recipeImgArr,recipUrlArr;
+
+
+@synthesize key1NameArr,key2NameArr,key3NameArr,key4NameArr;
+@synthesize key1ImgArr,key2ImgArr,key3ImgArr,key4ImgArr;
+@synthesize key1UrlArr,key2UrlArr,key3UrlArr,key4UrlArr;
+//初期化
 -(id)init
 {
-    recipeNameArr = [NSMutableArray array];
-    recipeImgArr = [NSMutableArray array];
-    recipUrlArr = [NSMutableArray array];
+    //破棄
+    //recipeNameArr = [NSMutableArray array];
+    //recipeImgArr = [NSMutableArray array];
+    //recipUrlArr = [NSMutableArray array];
+    
+    key1NameArr = [NSMutableArray array];
+    key1ImgArr = [NSMutableArray array];
+    key1UrlArr = [NSMutableArray array];
+    
+    key2NameArr = [NSMutableArray array];
+    key2ImgArr = [NSMutableArray array];
+    key2UrlArr = [NSMutableArray array];
+    
+    key3NameArr = [NSMutableArray array];
+    key3ImgArr = [NSMutableArray array];
+    key3UrlArr = [NSMutableArray array];
+    
+    key4NameArr = [NSMutableArray array];
+    key4ImgArr = [NSMutableArray array];
+    key4UrlArr = [NSMutableArray array];
     return self;
 }
 
 -(void)SetEventAndMoody:(NSInteger)eventindex moody:(NSInteger)moodyindex
 {
-    NSMutableArray *keyword = [NSMutableArray array];
+    //キーワード格納する配列
+    NSMutableArray *keyword1 = [NSMutableArray array];
+    NSMutableArray *keyword2 = [NSMutableArray array];
+    NSMutableArray *keyword3 = [NSMutableArray array];
+    NSMutableArray *keyword4 = [NSMutableArray array];
+    //ローカルファイル読み込み
     NSBundle *bundle = [NSBundle mainBundle];
     NSString *path = [bundle pathForResource:@"MARUREJson" ofType:@"json"];
     NSFileHandle *filehandle = [NSFileHandle fileHandleForReadingAtPath:path];
     NSData *data = [filehandle readDataToEndOfFile];
-    
+    //データセット
     if (data) {
         NSError * jsonErr = nil;
         NSDictionary * dic = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingAllowFragments error:&jsonErr];
-        
-        
-        
+        //イベントのセット
         NSArray* arrayEvent =[dic objectForKey:@"event"];
         NSDictionary * eventDic = [arrayEvent objectAtIndex:eventindex];
-        
+        //キーワードを検索
         NSString* _eventName = [eventDic objectForKey:@"name"];
         NSString* _ekey1 = [eventDic objectForKey:@"key1"];
         NSString* _ekey2 = [eventDic objectForKey:@"key2"];
         NSString* _ekey3 = [eventDic objectForKey:@"key3"];
+        NSString* _ekey4 = [eventDic objectForKey:@"key4"];
         
-        
+        //キーワードセット
         eventName = _eventName;
         self->eventName = _eventName;
         self->ekey1 = _ekey1;
         self->ekey2 = _ekey2;
         self->ekey3 = _ekey3;
+        self->ekey4 = _ekey4;
         
-        if (_ekey1.length > 0){[keyword addObject:_ekey1];}
-        if (_ekey2.length > 0){[keyword addObject:_ekey2];}
-        if (_ekey3.length > 0){[keyword addObject:_ekey3];}
+        //キーワードが空白出はない時セットする
+        if (_ekey1.length > 0){[keyword1 addObject:_ekey1];}
+        if (_ekey2.length > 0){[keyword2 addObject:_ekey2];}
+        if (_ekey3.length > 0){[keyword3 addObject:_ekey3];}
+        if (_ekey4.length > 0){[keyword4 addObject:_ekey4];}
         
+        //雰囲気のセット　上と同じような処理
         NSArray* arrayMoody = [dic objectForKey:@"moody"];
         
         NSDictionary * moodyDic = [arrayMoody objectAtIndex:moodyindex];
@@ -62,16 +93,18 @@
         NSString* _mkey1 = [moodyDic objectForKey:@"key1"];
         NSString* _mkey2 = [moodyDic objectForKey:@"key2"];
         NSString* _mkey3 = [moodyDic objectForKey:@"key3"];
-        
+        NSString* _mkey4 = [moodyDic objectForKey:@"key4"];
         
         self->moodyName = _moodyName;
         self->mkey1 = _mkey1;
         self->mkey2 = _mkey2;
         self->mkey3 = _mkey3;
+        self->mkey4 = _mkey4;
         
-        if (_mkey1.length > 0){[keyword addObject:_mkey1];}
-        if (_mkey2.length > 0){[keyword addObject:_mkey2];}
-        if (_mkey3.length > 0){[keyword addObject:_mkey3];}
+        if (_mkey1.length > 0){[keyword1 addObject:_mkey1];}
+        if (_mkey2.length > 0){[keyword2 addObject:_mkey2];}
+        if (_mkey3.length > 0){[keyword3 addObject:_mkey3];}
+        if (_mkey4.length > 0){[keyword4 addObject:_mkey4];}
         
     }
     else
@@ -79,16 +112,33 @@
         NSLog(@"file is missing.");
     }
     
-    
-    for (NSString *str in keyword)
+    const float intervalTime = 0.9;//   webapi検索のため最小間隔時間
+    //キーワードによる検索
+    for (NSString *str in keyword1)
     {
-        [self WebSerchApi:str];
-        [NSThread sleepForTimeInterval:1.0];
+        [self WebSerchApi:str nameArr:key1NameArr imgArr:key1ImgArr urlArr:key1UrlArr];
+        [NSThread sleepForTimeInterval:intervalTime];
+    }
+    
+    for (NSString *str in keyword2)
+    {
+        [self WebSerchApi:str nameArr:key2NameArr imgArr:key2ImgArr urlArr:key2UrlArr];
+        [NSThread sleepForTimeInterval:intervalTime];
+    }
+    for (NSString *str in keyword3)
+    {
+        [self WebSerchApi:str nameArr:key3NameArr imgArr:key3ImgArr urlArr:key3UrlArr];
+        [NSThread sleepForTimeInterval:intervalTime];
+    }
+    for (NSString *str in keyword4)
+    {
+        [self WebSerchApi:str nameArr:key4NameArr imgArr:key4ImgArr urlArr:key4UrlArr];
+        [NSThread sleepForTimeInterval:intervalTime];
     }
     
 }
 
--(void)WebSerchApi:(NSString *)keyword
+-(void)WebSerchApi:(NSString *)keyword nameArr:(NSMutableArray *)nameArr imgArr:(NSMutableArray *)imgArr urlArr:(NSMutableArray *)urlArr
 {
     //~~~~~~~~~~テスト　ローカルファイルを使う~~~~~~~~~~~~~~~
     
@@ -104,7 +154,7 @@
      NSURL *jsonUrl = [NSURL URLWithString:str];
      NSData *data = [NSData dataWithContentsOfURL:jsonUrl options:kNilOptions error:nil];
      */
-    
+    //検索結果をセット
     if(data)
     {
         NSError * jsonErr = nil;
@@ -119,12 +169,18 @@
             NSString *recipURL      = [resultDic objectForKey:@"recipeUrl"];
             NSString *recipImage    = [resultDic objectForKey:@"foodImageUrl"];
             
-            [self->recipeNameArr addObject:recipName];
-            [self->recipeImgArr addObject:recipImage];
-            [self->recipUrlArr addObject:recipURL];
+            //破棄
+            //[self->recipeNameArr addObject:recipName];
+            //[self->recipeImgArr addObject:recipImage];
+            //[self->recipUrlArr addObject:recipURL];
+            
+            //引数の配列にセット
+            [nameArr addObject:recipName];
+            [imgArr addObject:recipImage];
+            [urlArr addObject:recipURL];
             
             NSLog(@"key = %@ \n",keyword);
-            NSLog(@"name = %@ \n",recipName);
+            //NSLog(@"name = %@ \n",recipName);
             
         }
         
